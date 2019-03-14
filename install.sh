fc87a29bc3c27d6f4c1b6b0a8443a8d457533980
do-su-0805 <do.su.0805.engineer@gmail.com>
2019-03-15 00:16:24 +0900

    add overwrite option

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 ## for .gitconfig
 if [ ! -f ${HOME}/.gitconfig ]; then
-  ln -s `pwd`/.gitconfig ${HOME}/.gitconfig
+  ln -fs `pwd`/.gitconfig ${HOME}/.gitconfig
 fi
 
 ## for .screenrc
 if [ ! -f ${HOME}/.screenrc ]; then
-  ln -s `pwd`/.screenrc ${HOME}/.screenrc
+  ln -fs `pwd`/.screenrc ${HOME}/.screenrc
 fi
